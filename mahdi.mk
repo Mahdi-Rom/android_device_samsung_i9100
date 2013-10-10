0df5c67fd6961f3d07761d2daf813a10a9ad5385
Altaf-Mahdi <altaf.mahdi@gmail.com>
2013-10-10 14:25:28 +0100

    add mahdi.mk

---
+++ mahdi.mk
@@ -0,0 +1,26 @@
+# Release name
+PRODUCT_RELEASE_NAME := GT-I9100
+
+# Inherit AOSP device configuration.
+$(call inherit-product, device/samsung/i9100/full_i9100.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/mahdi/configs/common.mk)
+$(call inherit-product, vendor/mahdi/configs/common_full_phone.mk)
+
+# Inherit GSM files.
+$(call inherit-product, vendor/mahdi/configs/gsm.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := mahdi_i9100
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := i9100
+PRODUCT_MODEL := GT-I9100
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 TARGET_DEVICE=GT-I9100 BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.1.2/JZO54K:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 4.1.2 JZO54K release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/mahdi/prebuilt/bootanimations/BOOTANIMATION-800x480.zip:system/media/bootanimation.zip
